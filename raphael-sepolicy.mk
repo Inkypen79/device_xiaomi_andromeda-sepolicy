56ac59fbf811e1df8bfb5e18d188162b403cc19c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-02-06 10:33:01 +0530

    raphael-sepolicy: Initial sepolicy rules for raphael vendor

---
+++ raphael-sepolicy.mk
@@ -0,0 +1,26 @@
+# Qcom Sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#	device/xiaomi/raphael-sepolicy/vendor/qcom
+
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIRS += \
+#    device/xiaomi/raphael-sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIRS += \
+    device/xiaomi/raphael-sepolicy/private
+
+# NXP Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/raphael-sepolicy/vendor/nxp
+
+# Xiaomi Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/audio \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/battery \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/camera \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/fingerprint \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/light \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/motor \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/power \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/thermald \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/usb \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/wlan
