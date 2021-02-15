ca651f7ed2051d4cf5688a52b32c5ba20f4fcd70
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-02-15 11:58:46 +0100

    raphael-sepolicy: Address rules for Alipay
    Change-Id: I5a4acff5a059f5010b58ea230c714296827cddd0

--- raphael-sepolicy.mk
+++ raphael-sepolicy.mk
@@ -25,6 +25,7 @@ BOARD_SEPOLICY_DIRS += \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/fod \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/light \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/motor \
+    device/xiaomi/raphael-sepolicy/vendor/xiaomi/mlipay \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/parts \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/power \
     device/xiaomi/raphael-sepolicy/vendor/xiaomi/thermald \
