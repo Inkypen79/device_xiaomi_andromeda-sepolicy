db7fe087c25ff554e09923d4cd76bf5e91d210b2
Nicholas Magill <nmagill1979@gmail.com>
2021-04-29 11:11:36 +0100

    Initial andromeda bringup
    Change-Id: I159e99ad36048298e0c548acd6ba24f744841a1b

---
+++ sepolicy.mk
@@ -0,0 +1,52 @@
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIRS += \
+#    device/xiaomi/andromeda-sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/xiaomi/andromeda-sepolicy/private
+
+# NXP Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/andromeda-sepolicy/vendor/nxp
+
+# Lineage Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/andromeda-sepolicy/vendor/lineage
+
+# Qcom Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/andromeda-sepolicy/vendor/qcom
+
+# Xiaomi Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/audio \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/battery \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/bluetooth \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/camera \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/charge_logger \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/citsensorservice \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/displaycount \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/dumpstate \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/fingerprint \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/init \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/last_kmsg \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/light \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/mdm_helper \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/mfidoca \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/misys \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/mlipay \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/modprobe \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/mtdservice \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/net \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/parts \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/power \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/proc_net \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/radio \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/sensors \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/slaservice \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/thermald \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/tidaservice \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/usb \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/vsimapp \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/wifi_mac_generator \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/wireless \
+    device/xiaomi/andromeda-sepolicy/vendor/xiaomi/wlan
